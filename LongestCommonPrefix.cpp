63f0fb689e09cffeb2a384073c32602b8a0a8737
Oleksandr <61288324+lkn2007@users.noreply.github.com>
2020-05-18 15:23:16 +0300

    Update

--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -2,24 +2,12 @@ class Solution {
 public:
 	string longestCommonPrefix(vector<string>& strs)
 	{
-		if (strs.size() == 0)
+	   if (strs.size() == 0) {return "";}		
+	   string min_length_str = *min_element(strs.begin(), strs.end(), [](string a, string b) {return a.size() < b.size();});
+	   string prefix;
+		for (size_t i = 0; i < min_length_str.size(); ++i)
 		{
-			return "";
-		}
-		
-		size_t min_length_str = strs[0].size();
-		for (size_t i = 1; i < strs.size(); i++)
-		{
-			if ( strs[i].size() <= min_length_str)
-			{
-				min_length_str = strs[i].size();
-			}
-		}
-
-		string prefix;
-		for (size_t i = 0; i < min_length_str; i++)
-		{
-			for (size_t j = 0; j < strs.size(); j++)
+			for (size_t j = 0; j < strs.size(); ++j)
 			{
 				if (strs[j][i] != strs[0][i])
 				{
