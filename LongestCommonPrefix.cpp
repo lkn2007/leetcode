03ca5952590f944e67a00497010834b7f6cb7e29
Oleksandr <61288324+lkn2007@users.noreply.github.com>
2020-05-18 15:28:22 +0300

    Udate

--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -3,9 +3,9 @@ public:
 	string longestCommonPrefix(vector<string>& strs)
 	{
 	   if (strs.size() == 0) {return "";}		
-	   string min_length_str = *min_element(strs.begin(), strs.end(), [](string a, string b) {return a.size() < b.size();});
+	   string min_lenth_str = *min_element(strs.begin(), strs.end(), [](string a, string b) {return a.size() < b.size();});
 	   string prefix;
-		for (size_t i = 0; i < min_length_str.size(); ++i)
+		for (size_t i = 0; i < min_lenth_str.size(); ++i)
 		{
 			for (size_t j = 0; j < strs.size(); ++j)
 			{
